a3e52e68cd7c811a9932aadb53406e446eb938d2
AkshataMohanty <akshatamohanty@gmail.com>
2015-03-08 21:13:39 +0530

    final commit - 4 R files, 4 PNG files, Source ZIP file

---
+++ plot4.R
@@ -0,0 +1,74 @@
+## Plot 4 - Multiple Panel Plotting
+
+##  Setting working directory
+setwd("C://GitHub//ExData_Plotting1")
+
+##  Loading the data from the online source
+##  Dataset : Electrical Power Consumption - present in the working directory folder as a zip file
+
+##  Getting the column names of the data and extracting into vector
+colNames <- read.table(unz("exdata-data-household_power_consumption.zip", 
+                           "household_power_consumption.txt"), sep=";", na.strings="?",
+                       nrows=1, stringsAsFactors = FALSE)
+colNames <- as.character(as.vector(colNames[1,]))
+
+##  Data required from 2007-02-01 and 2007-02-02
+##  By observation of the table, starting row is 66638 till 60 * 24 * 2 = 2880 rows after
+##  This is because it's a two day period with 24 hours per day, 60 minutes each hour
+
+data <- read.table(unz("exdata-data-household_power_consumption.zip", 
+                       "household_power_consumption.txt"), sep=";", na.strings="?",
+                   nrows=2880, skip = 66637, col.names = colNames, header=TRUE)
+
+##  Renaming the columns appropriately
+colnames(data) <- colNames
+
+## Removing extra variables 
+rm(colNames)
+
+##  Converting Date and Time variables to Date/Time classes
+data$Date <- as.Date(data$Date, "%d/%m/%Y")
+
+##  Saving to png file - opening png device first
+png(filename = "plot4.png",
+    width = 480, height = 480, units = "px")
+
+##  Plot 4 - Multiple Panel Plotting
+par(mfrow=c(2,2))
+  ##  Panel 1
+  plot(data$Global_active_power, type="l", 
+       ylab="Global Active Power", xaxt="n", xlab="")
+    ##  Formating axis
+    axis(1, at=seq(1,2881,1440), labels=c("Thu", "Fri", "Sat"))
+ 
+  ##  Panel 2
+  plot(data$Voltage, type="l", xaxt="n",
+       xlab="datetime", ylab="Voltage")
+    ##  Formating axis
+    axis(1, at=seq(1,2881,1440), labels=c("Thu", "Fri", "Sat")) 
+
+  ##  Panel 3
+    ##  Submetering 1
+    plot(data$Sub_metering_1, 
+     type="l", xaxt="n", ylab="Energy sub metering", xlab="")
+    ##  Submetering 2
+    lines(data$Sub_metering_2, 
+      type="l", col="RED")
+    ##  Submetering 3
+    lines(data$Sub_metering_3, 
+      type="l", col="BLUE")
+    ##  Formating axis
+    axis(1, at=seq(1,2881,1440), labels=c("Thu", "Fri", "Sat"))
+    ##  Legend
+    legend('topright', c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3") , 
+       lty=1, col=c('black', 'red', 'blue'), bty="n", cex=1)
+ 
+  ##  Panel 4
+    plot(data$Global_reactive_power, type="l", 
+         xaxt="n", 
+         xlab="datetime", ylab="Global_reactive_power")
+    ##  Formating axis
+    axis(1, at=seq(1,2881,1440), labels=c("Thu", "Fri", "Sat"))
+
+##  Switching device off
+dev.off()
